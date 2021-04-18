1bef0be06f770d681cc33ad48ebccebd7c5a7b7a
Oksana Globazh <oksanaglobaz9@gmail.com>
2021-04-18 20:25:37 +0400

    Второе задание

--- script1.js
+++ script1.js
@@ -2,4 +2,13 @@
 
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-alert(numberOfFilms);
+
+let personalMovieDB = {
+    count: alert(numberOfFilms),
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+
+};
+
