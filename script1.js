222c975bab4daf2a54f655db85f4c4b876e002ea
Oksana Globazh <oksanaglobaz9@gmail.com>
2021-04-22 20:39:44 +0400

    Выполнено 2 задание

--- script1.js
+++ script1.js
@@ -1,25 +1,64 @@
 'use sctrict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+// let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
 
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-};
+// let cat = prompt('Один из последних просмотренных фильмов?');
+//     dog = +prompt('На сколько оцените его?');
+//     a = prompt('Один из последних просмотренных фильмов?');
+//     b = +prompt('На сколько оцените его?');
 
-let cat = prompt('Один из последних просмотренных фильмов?');
-    dog = +prompt('На сколько оцените его?');
-    a = prompt('Один из последних просмотренных фильмов?');
-    b = +prompt('На сколько оцените его?');
-
-personalMovieDB.movies[cat] = dog;
-personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[cat] = dog;
+// personalMovieDB.movies[a] = b;
 
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
+let numberOfFilms;
+
+    do {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    } while (numberOfFilms == '' || numberOfFilms == null);
+
+    // let = personalMovieDB.count;
+
+if (numberOfFilms < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (numberOfFilms > 10 || numberOfFilms < 30) {
+    alert("Вы классический зритель");
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман!');
+} else {
+    alert("Произошла ошибка");
+}
+
+let cat;
+
+    do {
+        cat = prompt('Один из последних просмотренных фильмов?');
+    } while (cat == '' || cat == null);
+
+let dog;
+
+    do {
+        dog = +prompt('На сколько оцените его?');
+    } while (dog == '' || dog == null);
+
+
+
+
+
+
+
+
+
+    
+    
