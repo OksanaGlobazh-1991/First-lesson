6f34d6988da44665a8730c295843816a216f69ae
Oksana Globazh <oksanaglobaz9@gmail.com>
2021-04-27 01:20:02 +0400

    добавлено третье задание

--- script1.js
+++ script1.js
@@ -41,49 +41,9 @@ for (i=0; i < 2; i++) {
     } while (dog == '' || dog == null);
     personalMovieDB.movies[cat] = dog;
 }
-// let cat;
-
-//     do {
-//         cat = prompt('Один из последних просмотренных фильмов?');
-//     } while (cat == '' || cat == null || cat.length < 50);
-
-// let dog;
-
-//     do {
-//         dog = +prompt('На сколько оцените его?');
-//     } while (dog == '' || dog == null);
 
 console.log(personalMovieDB);
 
-// let numberOfFilms;
-
-//     do {
-//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//     } while (numberOfFilms == '' || numberOfFilms == null);
-
-//     // let = personalMovieDB.count;
-
-// if (numberOfFilms < 10) {
-//     alert("Просмотрено довольно мало фильмов");
-// } else if (numberOfFilms > 10 || numberOfFilms < 30) {
-//     alert("Вы классический зритель");
-// } else if (numberOfFilms > 30) {
-//     alert('Вы киноман!');
-// } else {
-//     alert("Произошла ошибка");
-// }
-
-// let cat;
-
-//     do {
-//         cat = prompt('Один из последних просмотренных фильмов?');
-//     } while (cat == '' || cat == null || cat.length < 50);
-
-// let dog;
-
-//     do {
-//         dog = +prompt('На сколько оцените его?');
-//     } while (dog == '' || dog == null);
 
 
 
